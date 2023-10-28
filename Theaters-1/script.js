7f2003008c55bba762af7775831e8515a2c38068
Dilip <dilip.884400@gmail.com>
2023-10-28 19:27:00 +0530

    seat selecting according to count

--- Theaters-1/script.js
+++ Theaters-1/script.js
@@ -4,44 +4,64 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const ticketSelect = document.getElementById('ticket');
 const bookButton = document.querySelector('.book-tickets');
+const numTicketsSelect = document.getElementById('num-tickets');
 
 const premiumSeats = document.querySelectorAll('.premium .seat');
 const standardSeats = document.querySelectorAll('.standard .seat');
 
 let ticketPrice = +ticketSelect.value;
+let selectedSeatsCount = 0;
 
 populateUI();
 updateSelectedCount();
 
 function setMovieData(movieIndex, moviePrice) {
     localStorage.setItem('selectedMovieIndex1', movieIndex);
+    localStorage.setItem('selectedMoviePrice1', moviePrice);
 }
 
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 bookButton.addEventListener('click', () => {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    if (selectedSeatsCount === numTicketsSelect.value) {
+        const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    selectedSeats.forEach((seat) => {
-        seat.classList.remove('selected');
-        seat.classList.add('occupied');
-        seat.style.pointerEvents = 'none';
-    });
+        selectedSeats.forEach((seat) => {
+            seat.classList.remove('selected');
+            seat.classList.add('occupied');
+            seat.style.pointerEvents = 'none';
+        });
 
-    const storedSeatsIndexes = JSON.parse(localStorage.getItem('selectedSeatsIndexes')) || [];
+        const storedSeatsIndexes = JSON.parse(localStorage.getItem('selectedSeatsIndexes')) || [];
 
-    const selectedSeatsIndexes = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-    const updatedSeatsIndexes = [...storedSeatsIndexes, ...selectedSeatsIndexes];
-    localStorage.setItem('selectedSeatsIndexes', JSON.stringify(updatedSeatsIndexes));
+        const selectedSeatsIndexes = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+        const updatedSeatsIndexes = [...storedSeatsIndexes, ...selectedSeatsIndexes];
+        localStorage.setItem('selectedSeatsIndexes', JSON.stringify(updatedSeatsIndexes));
 
-    updateSelectedCount();
+        selectedSeatsCount = 0; 
+        updateSelectedCount();
+    } else {
+        const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+        selectedSeats.forEach((seat) => {
+            seat.classList.remove('selected');
+            seat.classList.add('occupied');
+            seat.style.pointerEvents = 'none';
+        });
+
+        const storedSeatsIndexes = JSON.parse(localStorage.getItem('selectedSeatsIndexes')) || [];
+
+        const selectedSeatsIndexes = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+        const updatedSeatsIndexes = [...storedSeatsIndexes, ...selectedSeatsIndexes];
+        localStorage.setItem('selectedSeatsIndexes', JSON.stringify(updatedSeatsIndexes));
+
+        selectedSeatsCount = 0; 
+        updateSelectedCount();
+    }
 });
-
 
 function populateUI() {
     const selectedSeatsIndexes = JSON.parse(localStorage.getItem('selectedSeatsIndexes'));
@@ -84,28 +104,17 @@ ticketSelect.addEventListener('change', (e) => {
 
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected');
-        updateSelectedCount();
+        if (selectedSeatsCount < numTicketsSelect.value) {
+            e.target.classList.toggle('selected');
+            selectedSeatsCount += e.target.classList.contains('selected') ? 1 : -1;
+            updateSelectedCount();
+        }
     }
 });
 
-bookButton.addEventListener('click', () => {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
-    selectedSeats.forEach((seat) => {
-        seat.classList.remove('selected');
-        seat.classList.add('occupied');
-        seat.style.pointerEvents = 'none';
-    });
-    
-    const selectedSeatsIndexes = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
-    const storedSeats = JSON.parse(localStorage.getItem('selectedSeats1')) || [];
-
-    storedSeats.push(...selectedSeatsIndexes);
-
-    localStorage.setItem('selectedSeats1', JSON.stringify(storedSeats));
-
+numTicketsSelect.addEventListener('change', (e) => {
+    // Update the selected seats count when the number of tickets select changes
+    selectedSeatsCount = 0;
     updateSelectedCount();
 });
 
